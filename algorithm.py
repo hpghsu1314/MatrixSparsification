09da2cc2f7984392650dc1c8e4784492ca239b0b
hpghsu1314 <143025880+hpghsu1314@users.noreply.github.com>
2023-11-11 14:04:12 -0800

    Delete algorithm.py

--- algorithm.py
+++
@@ -1,7 +0,0 @@
-import UVW
-
-#input size of the matrix to test
-#create UVW output for it
-    #Develop a method to check for "correctness"
-    #run the algorithm and act upon the correctness (AI implementation)
-
